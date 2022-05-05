d6cad9bfa39070361a1f9e7d75a9b06a60fd55d7
Dascal Tiberiu Claudiu <tiberiu.purcel@gmail.com>
2022-05-05 15:46:08 +0300

    Am facut noi modificari si teste pe fisierele C

--- Introducere_In_Programarea_Calculatoarelor/EXEMPLE_DIN_CURS/operatori_pe_biti_si.c
+++ Introducere_In_Programarea_Calculatoarelor/EXEMPLE_DIN_CURS/operatori_pe_biti_si.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-void main(void)
-{
-	unsigned int n,p;
-	printf("Introdu un numar natural pozitiv n=");
-	scanf("%d",&n);
-	printf("Introdu pozitia de shiftare p=");
-	scanf("%d", &p);
-	n=n&(~(1<<p));
-	printf("%u\n", n);
+#include <stdio.h>
+
+int main(void) {
+  unsigned int n, p;
+  printf("Introdu un numar natural pozitiv n=");
+  scanf("%d", &n);
+  printf("Introdu pozitia de shiftare p=");
+  scanf("%d", &p);
+  n = n & (~(1 << p));
+  printf("%u\n", n);
+	
+	return 0;
 }
--- Introducere_In_Programarea_Calculatoarelor/EXEMPLE_DIN_CURS/sirul_lui_fibonaci.c
+++ Introducere_In_Programarea_Calculatoarelor/EXEMPLE_DIN_CURS/sirul_lui_fibonaci.c
@@ -1,19 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main(void)
-{
-	unsigned int n,a=1,b=1,c,afisate;
-	printf("Introdu cate numere din sirul lui fibonaci afisam: n= ");
-	scanf("%d", &n);
-	afisate=2;
-	printf("%u %u ", a,b);
-	while(afisate<n){
-		c=a+b;
-		printf("%u ", c);
-		afisate++;
-		a=b;
-		b=c;
-	}
-	printf("\n");
+int main(void) {
+  unsigned int n, a = 1, b = 1, c, afisate;
+  printf("Introdu cate numere din sirul lui fibonaci afisam: n = ");
+  scanf("%d", &n);
+  afisate = 2;
+  printf("%u %u \n", a, b);
+  while (afisate < n) {
+    c = a + b;
+    printf("%u \n", c);
+    afisate++;
+    a = b;
+    b = c;
+  }
 
+  return 0;
 }
